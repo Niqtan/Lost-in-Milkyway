8f2abb4e04251a50935a6812a473f0b195b69db6
Niq S. <niqban123@gmail.com>
2025-11-03 12:28:09 +0800

    added new spritesheet and tilemap

--- Scenes/MainScenes/path_finding_manager.gd
+++ Scenes/MainScenes/path_finding_manager.gd
@@ -48,7 +48,7 @@ func update_terrain_movement_values() -> void:
 func get_valid_path(start_position: Vector2i, end_position: Vector2i) -> Array[Vector2i]:
 	path_array.clear()
 	
-	for point in astar_grid.get_id_path(start_position, end_position):
+	for point in astar_grid.get_point_path(start_position, end_position):
 		var current_point: Vector2i = point
 		
 		current_point += astar_grid.cell_size / 2 as Vector2i
@@ -56,5 +56,6 @@ func get_valid_path(start_position: Vector2i, end_position: Vector2i) -> Array[V
 		path_array.append(current_point)
 	
 	return path_array	
+
 func get_cell_movement_cost(cell_position: Vector2i) -> int:
 	return tile_map_grid.get_cell_tile_data(cell_position).get_custom_data_by_layer_id(1)
