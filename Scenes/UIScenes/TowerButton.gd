a665138a28093314f3551bff56a784957379b370
Niq S. <niqban123@gmail.com>
2025-11-10 12:34:46 +0800

    merged together all of the tower placement gui and functionality

--- Scenes/UIScenes/TowerButton.gd
+++ Scenes/UIScenes/TowerButton.gd
@@ -1,9 +1,14 @@
+"""
+This script mainly exists 
+
+"""
+
 extends Button
 
 @export var tower_type: String = ""
 
 func _ready() -> void:
-	self.pressed.connect(_on_button_pressed)	
-	
+	self.pressed.connect(_on_button_pressed)
+		
 func _on_button_pressed() -> void:
-	TowerPlacer.select_tower(tower_type)
+	print(tower_type)
--- scene_handler.gd
+++ scene_handler.gd
@@ -1,11 +1,13 @@
 class_name Main
 extends Node
-
-@export var tower_packed_scene: PackedScene = TowerPlacer.selected_tower
+@onready var tower_placer_node = $"Gameplay UI/TowerPlacerManager"
 @export var building_manager: BuildManager = null
 @onready var tile_map_layer: TileMapLayer = $Foreground
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_mouse"):
 		var cell_position: Vector2i = tile_map_layer.local_to_map(tile_map_layer.get_local_mouse_position())
-		building_manager.place_tower(cell_position, tower_packed_scene)
+		if tower_placer_node.selected_tower:
+			building_manager.place_tower(cell_position, tower_placer_node)
+		else:
+			print_debug("No tower selected!")
