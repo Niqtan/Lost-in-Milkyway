31788edc5c7e683b6420a6ba6f123e11678107b1
Niq S. <niqban123@gmail.com>
2025-12-03 17:25:34 +0800

    debugged tower behavior

--- Scenes/Towers/Projectiles/lightning_attack.gd
+++ Scenes/Towers/Projectiles/lightning_attack.gd
@@ -10,6 +10,14 @@ func _ready() -> void:
 	$AnimatedSprite2D.play("strike")
 	if target and is_instance_valid(target):
 		self.look_at(target.global_position)
+		self.rotation -= deg_to_rad(90)
+		
+		var distance = global_position.distance_to(target.global_position)
+		var sprite_height = 64
+		
+		var scale_factor = distance / sprite_height
+		
+		$AnimatedSprite2D.scale.y = scale_factor
 		target.take_damage(constellation_damage)
 		
 	await $AnimatedSprite2D.animation_finished
--- Scenes/Towers/archer_tower.gd
+++ Scenes/Towers/archer_tower.gd
@@ -3,28 +3,38 @@ class_name ArcherTower extends BaseTower
 # Define how the archer tower will attack
 
 @onready var animated_sprite: AnimatedSprite2D = get_node("AnimatedSprite2D")
-
 var constellation_attack = preload("res://Scenes/Towers/Projectiles/star_attack.tscn")
 
+var target: Enemy = null
 
 func _ready():
 	# Set the attack cooldown
 	attack_cooldown = 0.8
 
-func attack_enemies(target: Enemy) -> void:
+func attack_enemies(temporary_variable_target: Enemy) -> void:
 	# Change the tower's rotation
 	# To make it so that its like 
 	# they're attacking the enemies
 	
 	
-	if target:
+	if temporary_variable_target:
 		# Need to make it so that it doesn't cross 
 		# other tiles
-		var constellation_attack_scene = constellation_attack.instantiate()
 		
-		get_parent().add_child(constellation_attack_scene)
-		constellation_attack_scene.z_index = 100
-		constellation_attack_scene.global_position = $Aim.global_position
-		constellation_attack_scene.target = target
+		target = temporary_variable_target
+		if not $AnimatedSprite2D.frame_changed.is_connected(_on_frame_change):
+			$AnimatedSprite2D.frame_changed.connect(_on_frame_change)
+
+		$AnimatedSprite2D.play("attack")
 		
 		$AnimatedSprite2D.flip_h = target.global_position.x < global_position.x
+
+
+func _on_frame_change():
+	var constellation_attack_scene = constellation_attack.instantiate()
+	constellation_attack_scene.z_index = 100
+	constellation_attack_scene.global_position = $Aim.global_position
+	constellation_attack_scene.target = target
+		
+	if $AnimatedSprite2D.frame == 2:
+		get_parent().add_child(constellation_attack_scene)
